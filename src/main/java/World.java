c2a8e6652b943290eec5e84e77f793c9914dce44
imhyunho99 <nahyunho1999@gmail.com>
2025-05-21 09:21:08 +0900

    five

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,1 +1,3 @@
-public class World { public static void run() { System.out.println("201914074"); } }
+public class World { public static void run() { System.out.println("201914074"); 
+public static void run2() { System.out.println("hello world"); }
+} }
